c8716bb6438c1e8fba1a4dd2bf2cabfb6be9ac60
marcotulioteles <marcotuliocivileng@gmail.com>
2021-07-29 22:46:52 -0300

    builded header and cards info

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards,
+} from './styles'
+
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/73261068?v=4' }}
+            />
+            <User>
+              <UserGreeting>Olá</UserGreeting>
+              <UserName>Marco Túlio</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas" 
+          amount="R$ 17.400,00" 
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard 
+          type="down"
+          title="Saidas" 
+          amount="R$ 1.259,00" 
+          lastTransaction="Última saída dia 13 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total" 
+          amount="R$ 16.141,00" 
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
+    </Container>
+  );
+}
