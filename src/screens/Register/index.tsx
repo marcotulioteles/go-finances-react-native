2b186c110b5aff6bcb5282d3eb81d57ed00de1ad
marcotulioteles <marcotuliocivileng@gmail.com>
2021-07-31 19:49:35 -0300

    start: building the category modal

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Input } from '../../components/Forms/Input';
+import { Button } from '../../components/Forms/Button';
 
 import {
   Container,
   Header,
   Title,
-  Form
+  Form,
+  Fields,
+  TransactionTypes,
 } from './styles';
+import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton';
+import { CategorySelect } from '../../components/Forms/CategorySelect';
 
 export function Register() {
+  const [transactionType, setTransactionType] = useState('');
+
+  function hanldeTransactionTypeSelect(type: 'up' | 'down') {
+    setTransactionType(type);
+  }
+
   return (
     <Container>
       <Header>
@@ -17,11 +28,34 @@ export function Register() {
       </Header>
 
       <Form>
-        <Input
-          placeholder="Nome"
-        />
-        <Input
-          placeholder="Preço"
+        <Fields>
+          <Input
+            placeholder="Nome"
+          />
+          <Input
+            placeholder="Preço"
+          />
+          <TransactionTypes>
+            <TransactionTypeButton
+              type='up'
+              title="Income"
+              onPress={() => hanldeTransactionTypeSelect('up')}
+              isActive={transactionType === 'up'}            
+            />
+            <TransactionTypeButton
+              type='down'
+              title="Outcome"
+              onPress={() => hanldeTransactionTypeSelect('down')}
+              isActive={transactionType === 'down'}            
+            />
+          </TransactionTypes>
+          <CategorySelect 
+            title="Categoria"
+          />
+        </Fields>
+
+        <Button
+          title="Enviar"
         />
       </Form>
     </Container>
