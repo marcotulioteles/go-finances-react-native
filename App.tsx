8ecd4ad188a39f32ea0d78200d210358cab1ef64
marcotulioteles <marcotuliocivileng@gmail.com>
2021-08-09 20:29:31 -0300

    builded static interface of login screen

--- App.tsx
+++ App.tsx
@@ -19,6 +19,8 @@ import {
 import theme from './src/global/Styles/theme'
 import { AppRoutes } from "./src/routes/app.routes";
 
+import { SignIn } from './src/screens/SignIn';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -34,7 +36,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content"/>
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
